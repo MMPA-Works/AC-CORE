97622e4d288cfbded2d6210289fb241810088c75
Ian Macabulos <iannmacabulos@gmail.com>
2026-03-07 19:03:20 +0800

    chore: set up project folder structure and base lazy loading routes

--- accore-frontend/src/app/app.routes.ts
+++ accore-frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.routes').then(m => m.adminRoutes)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./citizen/citizen.routes').then(m => m.citizenRoutes)
+  }
+]; 
