f007c1fa15d63d4cd33316cf1ca9f4f648495c6c
Ian Macabulos <iannmacabulos@gmail.com>
2026-03-10 18:15:39 +0800

    feat: implement image compression, zoneless signals, and barangay validation

---
+++ accore-backend/src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const ANGELES_CITY_BARANGAYS = [
+  'Agapito del Rosario', 'Amsic', 'Anunas', 'Balibago', 'Capaya', 
+  'Claro M. Recto', 'Cuayan', 'Cutcut', 'Cutud', 'Lourdes North West', 
+  'Lourdes Sur', 'Lourdes Sur East', 'Malabañas', 'Margot', 'Mining', 
+  'Ninoy Aquino', 'Pampang', 'Pandan', 'Pulung Cacutud', 'Pulung Maragul', 
+  'Pulungbulu', 'Salapungan', 'San Jose', 'San Nicolas', 'Santa Teresita', 
+  'Santa Trinidad', 'Santo Cristo', 'Santo Domingo', 'Santo Rosario', 
+  'Sapalibutad', 'Sapangbato', 'Tabun', 'Virgen Delos Remedios'
+];
+
+export const PRIORITY_COMMERCIAL_ZONES = [
+  'Balibago', 'Malabañas', 'Pulung Maragul', 'Cutcut', 
+  'Santo Rosario', 'Pampang', 'San Nicolas'
+];
