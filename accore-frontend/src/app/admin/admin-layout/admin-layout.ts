51b5448a6f2889c23bd9bd6ee4818bb67a77110a
Ian Macabulos <iannmacabulos@gmail.com>
2026-03-12 18:49:07 +0800

    feat: redesign sidebar to responsive

--- accore-frontend/src/app/admin/admin-layout/admin-layout.ts
+++ accore-frontend/src/app/admin/admin-layout/admin-layout.ts
@@ -2,21 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, SidebarComponent],
-  template: `
-    <div class="h-screen w-full flex overflow-hidden bg-gray-100 font-sans">
-      
-      <app-sidebar></app-sidebar>
-
-      <main class="flex-1 relative z-0 overflow-auto">
-        <router-outlet></router-outlet>
-      </main>
-
-    </div>
-  `
+  imports: [CommonModule, RouterModule, SidebarComponent, LucideAngularModule],
+  templateUrl: './admin-layout.html'
 })
 export class AdminLayout {}
