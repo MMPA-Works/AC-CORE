9bb70304ade2574c0beb8c26bd5a4622e7699d11
Ian Macabulos <iannmacabulos@gmail.com>
2026-03-14 17:22:40 +0800

    feat: sidebar picture

--- accore-frontend/src/app/admin/admin-layout/admin-layout.ts
+++ accore-frontend/src/app/admin/admin-layout/admin-layout.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar';
 import { LucideAngularModule } from 'lucide-angular';
+import { AdminFooter } from '../admin-footer/admin-footer';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, SidebarComponent, LucideAngularModule],
+  imports: [CommonModule, RouterModule, SidebarComponent, LucideAngularModule, AdminFooter],
   templateUrl: './admin-layout.html'
 })
 export class AdminLayout {}
