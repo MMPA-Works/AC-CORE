5cecfda9a082bb3961f0e3090a8d9e49b8ea4dc0
Ian Macabulos <iannmacabulos@gmail.com>
2026-03-15 13:23:32 +0800

    landing page

--- accore-frontend/src/app/citizen/home/home.ts
+++ accore-frontend/src/app/citizen/home/home.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmCardImports } from '@spartan-ng/helm/card';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+
+import { 
+  lucideMapPin, 
+  lucideCamera, 
+  lucideCheckCircle,
+  lucideConstruction,
+  lucideWaves,
+  lucideDroplets,
+  lucideTreePine,
+  lucideLightbulbOff
+} from '@ng-icons/lucide';
+
 import { CitizenFooterComponent } from '../components/citizen-footer/citizen-footer';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, HlmButtonImports, CitizenFooterComponent],
-  templateUrl: './home.html',
+  imports: [
+    CommonModule, 
+    RouterLink, 
+    HlmButtonImports,
+    HlmCardImports,
+    HlmIcon,
+    NgIcon,
+    CitizenFooterComponent
+  ],
+  providers: [
+    provideIcons({ 
+      lucideMapPin, 
+      lucideCamera, 
+      lucideCheckCircle,
+      lucideConstruction,
+      lucideWaves,
+      lucideDroplets,
+      lucideTreePine,
+      lucideLightbulbOff
+    })
+  ],
+  templateUrl: './home.html'
 })
-export class Home {}
+export class Home {
+
+}
